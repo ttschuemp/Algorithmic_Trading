26dda70101413bcbe8f2cf233f6fff767642b56b
tts <t.tschuemperlin01@gmail.com>
2022-03-02 22:10:41 +0100

    authentication

--- model.py
+++ model.py
@@ -8,3 +8,9 @@ Created on Wed Mar  2 15:57:32 2022
 
 import pandas as pd 
 import numpy as np
+
+from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
+from api_key_secret import api_key, api_secret
+
+
+client = Client(api_key, api_secret)
