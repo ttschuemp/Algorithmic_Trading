ec258cf55b789f7c01ac4f6ba975838ceb9a52e1
Pegoraro, Marco <marco.pegoraro@blkb.ch>
2023-04-11 07:50:55 +0200

    add:

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import pandas as pd
 from src.pairs_trading_backtrader import PairTradingStrategy
-from src.test_strategy_backtrader import MyStrategy
 from src.pairs_trading_functions import find_cointegrated_pairs
 from src.load_data import fetch_crypto_data, fetch_data
 from binance import Client
-from api_key_secret import api_key, api_secret
+from src.api_key_secret import api_key, api_secret
 import backtrader as bt
 
 
@@ -12,7 +11,7 @@ if __name__ == "__main__":
 
     cerebro = bt.Cerebro()
 
-    client = Client(api_key,api_secret)
+    client = Client(api_key,api_secret, {"verify": path_zert})
     client.API_URL = 'https://testnet.binance.vision/api'
     data = fetch_crypto_data(10, 2, client)
 
@@ -81,3 +80,5 @@ if __name__ == "__main__":
     
     # Plot if requested
     #cerebro.plot(volume=False, zdown=False)
+
+#%%
