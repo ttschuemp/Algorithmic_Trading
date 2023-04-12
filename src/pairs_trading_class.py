b3cef1efd2de76b9b42b58f1d0f7f0c8d20041ad
Pegoraro, Marco <marco.pegoraro@blkb.ch>
2023-04-12 16:24:53 +0200

    add:

--- src/pairs_trading_class.py
+++ src/pairs_trading_class.py
@@ -340,6 +340,10 @@ class PairsTrading(bt.Strategy):
 
         self.ols_slope = btind.OLS_Slope_InterceptN(self.data_a, self.data_b, period=self.params.window)
 
+    def log(self, txt, dt=None):
+        dt = dt or self.datas[0].datetime.date(0)
+        print("{} {}".format(dt.isoformat(), txt))
+
     def calc_hedge_ratio(self):
 
         hedge_ratio = self.ols_slope.slope[0]
@@ -364,7 +368,7 @@ class PairsTrading(bt.Strategy):
                 self.order_target_size(self.datas[1], -self.hedge_ratio * self.size)
             elif self.zscore > self.upper_bound:
                 # Sell the spread
-                #self.log("SELL SPREAD: A {} B {}".format(self.data_a[0], self.data_b[0]))
+                self.log("SELL SPREAD: A {} B {}".format(self.data_a[0], self.data_b[0]))
                 self.order_target_size(self.datas[0], -self.size)
                 self.order_target_size(self.datas[1], self.hedge_ratio * self.size)
 
