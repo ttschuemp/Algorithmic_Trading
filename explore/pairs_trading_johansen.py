a79e52a47a00a710658026dad66a805fc4c07030
Pegoraro, Marco <marco.pegoraro@blkb.ch>
2023-05-08 11:12:46 +0200

    add:

--- explore/pairs_trading_johansen.py
+++ explore/pairs_trading_johansen.py
@@ -133,12 +133,124 @@ class PairsTrading(bt.Strategy):
 
 
 
+#%%
+class PairsTrading_2(bt.Strategy):
+    params = (
+        ("window", None),
+        ("std_dev", None),
+        ("size", None)
+    )
+
+    def __init__(self):
+        self.data_a = self.datas[0].close
+        self.data_b = self.datas[1].close
+        self.data_c = self.datas[2].close
+
+        self.window = self.params.window
+        self.hedge_ratio = None
+        self.spread = None
+        self.zscore = None
+        self.upper_bound = self.params.std_dev
+        self.lower_bound = -self.params.std_dev
+        self.size = self.params.size
+        self.trade_size = 0
+
+        self.hedge_ratio_history = []
+        self.spread_history = []
+        self.zscore_history = []
 
 
+    def log(self, txt, dt=None):
+        dt = dt or self.datas[0].datetime.date(0)
+        print("{} {}".format(dt.isoformat(), txt))
+
+
+    def calc_hedge_ratio(self, data_a, data_b, data_c):
+
+        try:
+
+            result = coint_johansen(np.array([data_a, data_b, data_c]).T, det_order=0, k_ar_diff=1)
+
+            self.hedge_ratio = result.evec[0] / result.evec[0][0]
+            self.spread = data_a[-1] * self.hedge_ratio[0] + data_b[-1] * self.hedge_ratio[1] + data_c[-1] * self.hedge_ratio[2]
+            self.hedge_ratio_history.append(self.hedge_ratio)
+            self.spread_history.append(self.spread)
+
+            spread_mean = pd.Series(self.spread_history).rolling(self.params.window).mean().iloc[-1]
+            spread_std_dev = pd.Series(self.spread_history).rolling(self.params.window).std().iloc[-1]
+            self.zscore = (self.spread - spread_mean) / spread_std_dev
+            self.zscore_history.append(self.zscore)
+
+            return self.zscore
+
+        except Exception as e:
+
+            print(f"Error calculating hedge ratio: {e}")
+            return None
+
+
+
+    def start(self):
+        self.equity = self.broker.get_cash()
+
+
+    def next(self):
+
+        if len(self.data_a) >= self.window and len(self.data_b) >= self.window and len(self.data_c) >= self.window:
+
+            self.equity = self.broker.get_value()
+            self.trade_size = self.equity * self.params.size / self.data_a[0]
+
+            data_a = self.data_a.get(size=self.window)
+            data_b = self.data_b.get(size=self.window)
+            data_c = self.data_c.get(size=self.window)
+            self.zscore = self.calc_hedge_ratio(data_a, data_b, data_c)
+
+            # Check if there is already an open trade
+            if self.getposition().size == 0:
+                if (self.zscore < self.lower_bound):
+                    # Buy the spread
+                    self.log("BUY SPREAD: A {} B {}".format(self.data_a[0], self.data_b[0], self.data_c[0]))
+                    self.log("Z-SCORE: {}".format(self.zscore))
+                    self.log("Portfolio Value: {}".format(self.equity))
+
+                    self.order_target_size(self.datas[0], self.trade_size)
+                    self.order_target_size(self.datas[1], self.hedge_ratio[1] * self.trade_size)
+                    self.order_target_size(self.datas[2], self.hedge_ratio[2] * self.trade_size)
+
+                elif (self.zscore > self.upper_bound):
+                    # Sell the spread
+                    self.log("SELL SPREAD: A {} B {}".format(self.data_a[0], self.data_b[0], self.data_c[0]))
+                    self.log("Z-SCORE: {}".format(self.zscore))
+
+                    self.order_target_size(self.datas[0], -self.trade_size)
+                    self.order_target_size(self.datas[1], -self.hedge_ratio[1] * self.trade_size)
+                    self.order_target_size(self.datas[2], -self.hedge_ratio[2] * self.trade_size)
+
+
+            # If there is an open trade, wait until the zscore crosses zero
+            elif self.getposition().size > 0 and self.zscore > 0:
+                self.log("CLOSE LONG SPREAD: A {} B {}".format(self.data_a[0], self.data_b[0], self.data_c[0]))
+                self.log("Z-SCORE: {}".format(self.zscore))
+                self.log("Portfolio Value: {}".format(self.equity))
+                self.order_target_size(self.datas[0], 0)
+                self.order_target_size(self.datas[1], 0)
+                self.order_target_size(self.datas[2], 0)
+
+
+            elif self.getposition().size < 0 and self.zscore < 0:
+                self.log("CLOSE SHORT SPREAD: A {} B {}".format(self.data_a[0], self.data_b[0], self.data_c[0]))
+                self.log("Z-SCORE: {}".format(self.zscore))
+                self.log("Portfolio Value: {}".format(self.equity))
+                self.order_target_size(self.datas[0], 0)
+                self.order_target_size(self.datas[1], 0)
+                self.order_target_size(self.datas[2], 0)
 
 
 
 #%%
+
+# using 2 series
 
 # why are the winning and losing trades always the same amount?
 # ADF with trades most of time pvalue >0.05 -> non stationary. means spread we trade is not stationary
@@ -327,14 +439,36 @@ def find_cointegrated_pairs_johansen(data):
                     hedge_ratio = result.evec[0] / result.evec[0][0]
                     sum_of_traces = np.sum(result.lr1)
 
-                    pairs.append((keys[i], keys[j], keys[k], hedge_ratio, result.lr1, sum_of_traces))
+                    # calc spread
+                    spread = S1 * hedge_ratio[0] + S2 * hedge_ratio[1] + S3 * hedge_ratio[2]
+
+                    # calc half life
+                    prevz = spread.shift()
+                    dz = spread-prevz
+                    dz = dz[1:,]
+                    prevz = prevz[1:,]
+                    model2 = sm.OLS(dz, prevz-np.mean(prevz))
+                    results2 = model2.fit()
+                    theta = results2.params
+                    half_life = -np.log(2)/theta
+
+                    lags = range(2, len(spread)//2)
+                    tau = [np.sqrt(np.abs(pd.Series(spread) - pd.Series(spread).shift(lag)).dropna().var()) for lag in lags]
+                    poly = np.polyfit(np.log(lags), np.log(tau), 1)
+                    hurst_exp = poly[0] * 2
+
+                    hurst_2, c, data_2 = compute_Hc(spread, kind='change', simplified=True)
+
+                    pairs.append((keys[i], keys[j], keys[k], sum_of_traces, half_life.values, hurst_exp, hurst_2))
 
     # Sort cointegrated pairs by p-value in ascending order
-    pairs.sort(key=lambda x: x[5], reverse=True)
+    pairs.sort(key=lambda x: x[3], reverse=True)
 
-    return pd.DataFrame(pairs, columns=['Asset 1', 'Asset 2', 'Asset 3', 'hedge ratio', 'trace statistics', 'sum of traces'])
+    return pd.DataFrame(pairs, columns=['Asset 1', 'Asset 2', 'Asset 3',
+                                        'sum of traces', 'half life', 'hurst exp 1',
+                                        'hurst exp 2'])
 
-#%%
+#%% using 3 series
 if __name__ == "__main__":
     days = 180
     cerebro = bt.Cerebro()
@@ -343,5 +477,88 @@ if __name__ == "__main__":
     client = Client(api_key, api_secret)
     data = fetch_crypto_data(30, days, client)
     pairs = find_cointegrated_pairs_johansen(data)
+
+    window = int(pairs['half life'][0])
+    #window = 150
+    std_dev = 1
+    size = 0.02
+
+    # Choose the pair with the smallest p-value
+    tickers_pairs = pairs.iloc[0, 0:3]
+    print(f'trading pair: ' + str(tickers_pairs))
+
+    data_df0 = fetch_data(tickers_pairs[0], '1h', str(days * 24), client)
+    data_df1 = fetch_data(tickers_pairs[1], '1h', str(days * 24), client)
+    data_df2 = fetch_data(tickers_pairs[2], '1h', str(days * 24), client)
+
+    data0 = bt.feeds.PandasData(dataname=pd.DataFrame(data_df0))
+    data1 = bt.feeds.PandasData(dataname=pd.DataFrame(data_df1))
+    data2 = bt.feeds.PandasData(dataname=pd.DataFrame(data_df2))
+    cerebro.adddata(data0)
+    cerebro.adddata(data1)
+    cerebro.adddata(data2)
+
+    cerebro.addstrategy(PairsTrading_2, window=window, std_dev=std_dev, size=size)
+
+    # Set the commission and the starting cash
+    cerebro.broker.setcommission(commission=0.001)
+    cerebro.broker.setcash(100000)
+
+    # Add analyzers
+    cerebro.addanalyzer(bt.analyzers.TradeAnalyzer, _name='trade_analyzer')
+    cerebro.addanalyzer(bt.analyzers.PyFolio, _name='PyFolio')
+
+    # Run the backtest
+    results = cerebro.run()
+
+    final_value = cerebro.broker.getvalue()
+    print("Final portfolio value: ${}".format(final_value))
+
+    # Get the analyzers and print the results
+    trade_analyzer = results[0].analyzers.trade_analyzer.get_analysis()
+    print("Starting cash: ${}".format(cerebro.broker.startingcash))
+    print("Ending cash: ${}".format(cerebro.broker.getvalue()))
+    print("Total return: {:.2f}%".format(100*((cerebro.broker.getvalue()/cerebro.broker.startingcash)-1)))
+    print("Half-Live: " + str(window) + " hours")
+    print("Number of trades: {}".format(trade_analyzer.total.closed))
+    print("Winning Trades:", results[0].analyzers.trade_analyzer.get_analysis()['won']['total'])
+    print("Losing Trades:", results[0].analyzers.trade_analyzer.get_analysis()['lost']['total'])
+    print("Win Ratio:", results[0].analyzers.trade_analyzer.get_analysis()['won']['total'] /
+          trade_analyzer.total.closed)
+
+
+    strategy_instance = results[0]
+
+    # Plot the spread, zscore, and hedge ratio
+    plt.subplot(4, 1, 1)
+    plt.plot(strategy_instance.spread_history)
+    plt.title(f'Spread {list(tickers_pairs)}')
+
+    plt.subplot(4, 1, 2)
+    plt.plot(strategy_instance.zscore_history)
+    plt.axhline(strategy_instance.upper_bound, color='r')
+    plt.axhline(strategy_instance.lower_bound, color='r')
+    plt.title("Z-score")
+    plt.legend(["Z-score"])
+
+    plt.subplot(4, 1, 3)
+    plt.plot(strategy_instance.hedge_ratio_history)
+    plt.title("Hedge ratio")
+    plt.legend(["Hedge ratio"])
+
+    plt.tight_layout()
+    plt.savefig('strategy')
+    plt.show()
+
+    # create cerebro chart
+    cerebro.plot(iplot=True, volume=False)
+
+    # create quantstats charts & statistics html
+    portfolio_stats = strategy_instance.analyzers.getbyname('PyFolio')
+    returns, positions, transactions, gross_lev = portfolio_stats.get_pf_items()
+    returns.index = returns.index.tz_convert(None)
+
+    quantstats.reports.html(returns, output='stats.html', title='Backtrade Pairs')
+
 
 #%%
