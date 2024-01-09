d1227722b7992be357fc1b52396870e859c9e5c8
mpego <m.pegoraro93@gmail.com>
2024-01-09 21:03:06 +0100

    add: future

--- src/data/futures-data.py
+++ src/data/futures-data.py
@@ -11,24 +11,37 @@ api_secret = 'YYo9kaXiGttMdCeZwWxSQymhv8hnLsYPUiifPeapdM1t44ASdpot4fDR6ioJnz2h'
 
 client = Client(api_key, api_secret)
 
-symbol = 'BTCUSDT'
+symbol = 'ETHUSDT'
 interval = '1d'
 limit = 1000
 
-# fetch futures
-klines = client.futures_klines(symbol=symbol, interval=interval, limit=limit)
-df2 = pd.DataFrame(klines, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_asset_volume', 'number_of_trades', 'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume', 'ignore'])
-
-df2['timestamp'] = pd.to_datetime(df2['timestamp'], unit='ms')
-future_prices_close = df2['close'].astype(float)
-
-
 # fetch spot
-klines_spot = client.get_klines(symbol=symbol, interval=interval, limit=limit)
-df_spot = pd.DataFrame(klines, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_asset_volume', 'number_of_trades', 'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume', 'ignore'])
+klines_spot = client.get_klines(
+    symbol=symbol,
+    interval=interval,
+    limit=limit
+
+)
+df_spot = pd.DataFrame(klines_spot, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_asset_volume', 'number_of_trades', 'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume', 'ignore'])
 # Convert timestamp to readable date
 df_spot['timestamp'] = pd.to_datetime(df_spot['timestamp'], unit='ms')
-spot_prices_close = df_spot['close'].astype(float)
+spot_prices_close = df_spot['open'].astype(float)
 
-plt.plot(future_prices_close)
-plt.savefig('future price')
+
+r = client.futures_continous_klines(
+    pair=symbol,
+    contractType='CURRENT_QUARTER',
+    interval=interval,
+    limit=limit
+)
+df_future_next_quarter = pd.DataFrame(r, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_asset_volume', 'number_of_trades', 'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume', 'ignore'])
+# Convert timestamp to readable date
+df_future_next_quarter['timestamp'] = pd.to_datetime(df_future_next_quarter['timestamp'], unit='ms')
+future_next_quarter_close = df_future_next_quarter['open'].astype(float)
+
+
+plt.plot(pd.DataFrame(future_next_quarter_close/spot_prices_close).set_index(df_spot['timestamp']), color='blue')
+plt.axhline(1, color='r')
+plt.legend(["Spread " + f'{symbol}'])
+plt.rcParams.update({'font.size': 18})
+plt.show()
